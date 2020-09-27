ad2d00ad5d34d59285525cd1d4448bcf0b8e5ade
Faiz <faiz.anwar4749@gmail.com>
2020-09-28 01:19:50 +0530

    [Faiz] Refactor : Adding UseCase4

---
+++ SwitchCaseUC4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwitchCaseUC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
+            int EMP_WAGE_PER_HOUR = 20;
+            int EmpHours, EmpWage;
+            Random rand = new Random();
+            int check = rand.Next(1, 3);
+            switch (check)
+            {
+                case 2:
+                    EmpHours = 8;
+                    break;
+                case 1:
+                    EmpHours = 4;
+                    break;
+                default:
+                    EmpHours = 0;
+                    break;
+            }
+            EmpWage = EmpHours * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Employee Wages: " + EmpWage);
+        }
+    }
+}
