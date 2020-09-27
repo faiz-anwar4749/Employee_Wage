511fa0f7d8298da24acb2b06805cfb51d2464ef5
Faiz <faiz.anwar4749@gmail.com>
2020-09-28 01:16:12 +0530

    [Faiz] Refactor : Adding UseCase3

---
+++ PartTimeWagesUC3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PartTimeWagesUC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
+            int EMP_WAGE_PER_HOUR = 20;
+            int EmpHours, EmpWage;
+            Random rand = new Random();
+            int check = rand.Next(1, 3);
+            if (check == IS_FULL_TIME)
+            {
+                EmpHours = 8;
+            }
+            else if (check == IS_PART_TIME)
+            {
+                EmpHours = 4;
+            }
+            else
+                EmpHours = 0;
+            EmpWage = EmpHours * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Employee Wages: " + EmpWage);
+        }
+    }
+}
